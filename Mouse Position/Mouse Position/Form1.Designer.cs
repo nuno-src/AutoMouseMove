fa41fd78e3b7b5fa2675d4adc2ad9cd265f7ebb7
nuno <75589883+nunocardoso7@users.noreply.github.com>
2022-02-19 04:09:28 +0000

    add a mouse position made in C#

---
+++ Mouse Position/Mouse Position/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace Mouse_Position
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.labely = new System.Windows.Forms.Label();
+            this.labelx = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(81, 49);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "X:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(82, 127);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 29);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Y:";
+            // 
+            // labely
+            // 
+            this.labely.AutoSize = true;
+            this.labely.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labely.Location = new System.Drawing.Point(127, 127);
+            this.labely.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labely.Name = "labely";
+            this.labely.Size = new System.Drawing.Size(55, 29);
+            this.labely.TabIndex = 3;
+            this.labely.Text = "000";
+            // 
+            // labelx
+            // 
+            this.labelx.AutoSize = true;
+            this.labelx.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelx.Location = new System.Drawing.Point(127, 49);
+            this.labelx.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelx.Name = "labelx";
+            this.labelx.Size = new System.Drawing.Size(55, 29);
+            this.labelx.TabIndex = 2;
+            this.labelx.Text = "000";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(304, 216);
+            this.Controls.Add(this.labely);
+            this.Controls.Add(this.labelx);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label labely;
+        private System.Windows.Forms.Label labelx;
+    }
+}
+
---
+++ Mouse Position/Mouse Position/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace Mouse_Position
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           Thread positionThread = new Thread(Position);
+           positionThread.Start();
+        }
+        private void Position()
+        {
+            while (true)
+            {
+                int x = MousePosition.X;
+                int y = MousePosition.Y;
+
+                labelx.Text = x.ToString();
+                labely.Text = y.ToString();
+
+
+            }
+            
+        }
+    }
+}
