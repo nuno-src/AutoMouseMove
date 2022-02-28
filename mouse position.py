2d54954950b26cf005831def45c4bdca8cc75f05
nuno <75589883+nunocardoso7@users.noreply.github.com>
2022-02-28 19:33:30 +0000

    Update mouse position.py

--- mouse position.py
+++ mouse position.py
@@ -1,9 +1,21 @@
 import mouse
+import time
+import platform
+import os
+
+
+clear_command = "cls" if platform.system() == "Windows" else "clear"
+
+os.system(clear_command)
+
+
 
 
 def basic():
 
     while True:
+        os.system(clear_command)
         print("Current Mouse Position: ",mouse.get_position())
+        time.sleep(0.5)
 
 basic()
